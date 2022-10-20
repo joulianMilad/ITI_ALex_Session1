732e88b24a1dec8df78f1fdc150ab3e1728a5a6a
Fady Soliman <93131759+joulianMilad@users.noreply.github.com>
2022-10-20 20:27:24 +0200

    Update Main.java
    Fady

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String ip = "192.168.1.5";
+        String ip = "192.168.100.100";
         String [] ipSplitters = ip.split("\\.");
         for (String i:ipSplitters){
             System.out.println(i);
